f72e8d9665a262497641ff3e3e7bfc5461c9f9c0
bjgeumgang <bjgeumgang@gmail.com>
2025-10-02 08:52:21 +0900

    Link_useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,32 @@ import Home from './pages/Home';
 import Diary from './pages/Diary';
 import New from './pages/New';
 import Notfound from './pages/Notfound';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Link, useNavigate } from 'react-router-dom';
 // 1. "/": 모든 일기를 조회하는 Home 페이지
 // 2. "/new": 새로운 일기를 작성하는 New 페이지
 // 3. "/diary": 일기를 상세히 조회하는 diary 페이지
 
 function App() {
+  const nav = useNavigate();
+  const onClickButton = () => {
+    nav('/new');
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/new" element={<New />} />
-      <Route path="/diary" element={<Diary />} />
-      <Route path="*" element={<Notfound />} />
-    </Routes>
+    <>
+      <div>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/new'}>New</Link>
+        <Link to={'/diary'}>Diary</Link>
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </>
   );
 }
 
