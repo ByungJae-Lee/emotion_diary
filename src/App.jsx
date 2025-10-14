cd0a1a70da48ade63e32695aceaed30a306e69c4
bjgeumgang <bjgeumgang@gmail.com>
2025-10-14 09:23:42 +0900

    add_layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,8 @@ import Diary from './pages/Diary';
 import New from './pages/New';
 import Notfound from './pages/Notfound';
 import { Route, Routes, Link, useNavigate } from 'react-router-dom';
+import { getEmotionImage } from './util/get-emotion-image';
 
-import emotion1 from './assets/emotion1.png';
-import emotion2 from './assets/emotion2.png';
-import emotion3 from './assets/emotion3.png';
-import emotion4 from './assets/emotion5.png';
-import emotion5 from './assets/emotion5.png';
 // 1. "/": 모든 일기를 조회하는 Home 페이지
 // 2. "/new": 새로운 일기를 작성하는 New 페이지
 // 3. "/diary": 일기를 상세히 조회하는 diary 페이지
@@ -23,11 +19,11 @@ function App() {
   return (
     <>
       <div>
-        <img src={emotion1} />
-        <img src={emotion2} />
-        <img src={emotion3} />
-        <img src={emotion4} />
-        <img src={emotion5} />
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
       </div>
       <div>
         <Link to={'/'}>Home</Link>
