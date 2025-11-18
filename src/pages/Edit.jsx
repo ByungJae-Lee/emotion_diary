5d1603b7092b0e648f949358cc5b3b6eee157a75
bjgeumgang <bjgeumgang@gmail.com>
2025-11-18 20:22:21 +0900

    add_edit page

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+import Header from '../components/Header';
+import Button from '../components/Button';
+import Editor from '../components/Editor';
+import { useContext } from 'react';
+import { DiaryDispatchContext } from '../App';
 
 const Edit = () => {
   const params = useParams();
-  return <div>{params.id}번 일기 수정페이지 입니다.</div>;
+  const nav = useNavigate();
+  const { onDelete } = useContext(DiaryDispatchContext);
+
+  const onClickDelete = () => {
+    if (window.confirm('일기를 정말 삭제하겠습니까?')) {
+      onDelete(params.id);
+      nav('/', { replace: true });
+    }
+  };
+
+  return (
+    <div>
+      <Header
+        title={'일기 수정하기'}
+        leftChild={<Button onClick={() => nav(-1)} text={'뒤로 가기'} />}
+        rightChild={
+          <Button onClick={onClickDelete} text={'삭제하기'} type={'NEGATIVE'} />
+        }
+      />
+      <Editor />
+    </div>
+  );
 };
 
 export default Edit;
