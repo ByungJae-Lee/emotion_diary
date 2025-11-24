9c80f36aa7b9f03c621a781c46d4cc04af1bc593
bjgeumgang <bjgeumgang@gmail.com>
2025-11-24 22:26:20 +0900

    add_Diary_HeaderUI

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import Header from '../components/Header';
+import Button from '../components/Button';
 
 const Diary = () => {
   const params = useParams();
-  console.log(params);
-  return <div>{params.id}번째 일기입니다.</div>;
+
+  return (
+    <div>
+      <Header
+        title={'yyyy-mm-dd 기록'}
+        leftChild={<Button text={'< 뒤로 가기'} />}
+        rightChild={<Button text={'수정하기'} />}
+      />
+    </div>
+  );
 };
 
 export default Diary;
