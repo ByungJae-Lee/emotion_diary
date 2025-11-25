0d38747808eb995869c88a4b469962aa83bfe921
bjgeumgang <bjgeumgang@gmail.com>
2025-11-25 09:56:41 +0900

    add_Diary_emotion

--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import Header from '../components/Header';
 import Button from '../components/Button';
+import Viewer from '../components/Viewer';
 
 const Diary = () => {
   const params = useParams();
@@ -13,6 +14,7 @@ const Diary = () => {
         leftChild={<Button text={'< 뒤로 가기'} />}
         rightChild={<Button text={'수정하기'} />}
       />
+      <Viewer />
     </div>
   );
 };
