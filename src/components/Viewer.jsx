4ae04c5a7e98727ecbe93811b6e158629291b362
bjgeumgang <bjgeumgang@gmail.com>
2025-11-27 22:00:27 +0900

    add_Diary_Viewer2

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -18,7 +18,12 @@ const Viewer = () => {
           <div>{EmotionItem.emotionName}</div>
         </div>
       </section>
-      <section className="content_section"></section>
+      <section className="content_section">
+        <h4>오늘의 일기</h4>
+        <div className="content_wrapper">
+          <p>일기...</p>
+        </div>
+      </section>
     </div>
   );
 };
--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
 import Button from '../components/Button';
 import Viewer from '../components/Viewer';
 
 const Diary = () => {
   const params = useParams();
+  const nav = useNavigate();
 
   return (
     <div>
       <Header
         title={'yyyy-mm-dd 기록'}
-        leftChild={<Button text={'< 뒤로 가기'} />}
-        rightChild={<Button text={'수정하기'} />}
+        leftChild={<Button onClick={() => nav(-1)} text={'< 뒤로 가기'} />}
+        rightChild={
+          <Button onClick={() => `/edit/${params.id}`} text={'수정하기'} />
+        }
       />
       <Viewer />
     </div>
